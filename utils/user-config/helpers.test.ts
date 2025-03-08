43de810cafff3bfa0fadf12e9396e96dafc61e55
azqajafardev <azqajafar@gmail.com>
2025-03-08 09:38:00 +0500

    feat(config): implement migration for system prompt configuration

--- utils/user-config/helpers.test.ts
+++ utils/user-config/helpers.test.ts
@@ -6,6 +6,7 @@ import { storage } from 'wxt/utils/storage'
 import { resetFakeBrowser, resetFakeEntrypoint } from '@/tests/utils/fake-browser'
 
 import { ByteSize } from '../sizes'
+import { Config } from './helpers'
 
 describe('user config', () => {
   beforeEach(() => {
@@ -53,5 +54,81 @@ describe('user config', () => {
     const { _getUserConfig: getUserConfig } = await import('./index')
     const userConfig = await getUserConfig()
     expect(userConfig.llm.enableNumCtx.get()).toBe(true)
+  })
+
+  it('should migrate from old value', async () => {
+    resetFakeBrowser({
+      fakeSystemMemory: {
+        capacity: ByteSize.fromGB(16).toBytes(),
+        availableCapacity: ByteSize.fromGB(16).toBytes(),
+        usage: ByteSize.fromGB(0).toBytes(),
+      },
+    })
+
+    // first config version
+    const config1 = await new Config('testKey').default('defaultValue').build()
+    expect(config1.get()).toBe('defaultValue')
+
+    // user change the value of config
+    config1.set('newValue')
+
+    // new config version
+    const newConfig1 = await new Config('testKey_new')
+      .default('defaultValue')
+      .migrateFrom('testKey', (value) => {
+        // if user change the value to 'newValue' we preserve it to the new config
+        if (value === 'newValue') return value
+        // otherwise, clear the old config
+        return undefined
+      }).build()
+
+    expect(newConfig1.get()).toBe('newValue')
+
+    const newConfig2 = await new Config('testKey_new')
+      .default('defaultValue')
+      .migrateFrom('testKey', (value) => {
+        if (value === 'newValue') return 'migratedValue'
+        return value
+      }).build()
+
+    expect(newConfig2.get()).toBe('newValue')
+  })
+
+  it('should migrate from old value (2)', async () => {
+    resetFakeBrowser({
+      fakeSystemMemory: {
+        capacity: ByteSize.fromGB(16).toBytes(),
+        availableCapacity: ByteSize.fromGB(16).toBytes(),
+        usage: ByteSize.fromGB(0).toBytes(),
+      },
+    })
+
+    // first config version
+    const config1 = await new Config('testKey').default('defaultValue').build()
+    expect(config1.get()).toBe('defaultValue')
+
+    // user change the value of config
+    config1.set('someValue')
+
+    // new config version
+    const newConfig1 = await new Config('testKey_new')
+      .default('defaultValue')
+      .migrateFrom('testKey', (value) => {
+        // if user change the value to 'newValue' we preserve it to the new config
+        if (value === 'newValue') return value
+        // otherwise, clear the old config
+        return undefined
+      }).build()
+
+    expect(newConfig1.get()).toBe('defaultValue')
+
+    const newConfig2 = await new Config('testKey_new')
+      .default('defaultValue')
+      .migrateFrom('testKey', (value) => {
+        if (value === 'newValue') return 'migratedValue'
+        return value
+      }).build()
+
+    expect(newConfig2.get()).toBe('defaultValue')
   })
 })
